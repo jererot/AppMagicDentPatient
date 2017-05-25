ee1d3ec8508d1cbf553e68a552dbf87557f0cadd
jererot <jererot.roy@gmail.com>
2017-05-24 23:20:05 -0500

    CreateProject

---
+++ AppMagicDentPatient/AppMagicDentPatient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppMagicDentPatient.Startup))]
+namespace AppMagicDentPatient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
